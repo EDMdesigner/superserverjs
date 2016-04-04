ccc0bf62d2ac77e159a310a17cef3f2af0dd2c8d
Gyula Nemeth <gyula.nemeth85@gmail.com>
2016-04-04 13:30:45 +0200

    RED-4628 init

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require("gulp");
+var jscs = require("gulp-jscs");
+var jshint = require("gulp-jshint");
+var stylish = require("gulp-jscs-stylish");
+var jsonlint = require("gulp-jsonlint");
+
+var jsFiles = [
+	"./**/*.js",
+	"!node_modules/**/*",
+	"!./**/*.built.js"
+];
+
+var jsonFiles = [
+	".jshintrc",
+	".jscsrc",
+	"!node_modules/**/*",
+	"./**/*.json"
+];
+
+// JSON lint
+// ==================================================
+gulp.task("jsonlint", function() {
+	return gulp.src(jsonFiles)
+		.pipe(jsonlint())
+		.pipe(jsonlint.failOnError());
+});
+
+
+// JS Hint
+// ==================================================
+gulp.task("jshint", function() {
+	return gulp.src(jsFiles)
+		.pipe(jshint(".jshintrc"))
+		.pipe(jshint.reporter("jshint-stylish"))
+		.pipe(jshint.reporter("fail"));
+});
+
+
+// JS CodeStyle
+// ==================================================
+gulp.task("jscs", function() {
+	return gulp.src(jsFiles)
+		.pipe(jscs({
+			configPath: ".jscsrc",
+			fix: true
+		}))
+		.pipe(stylish())
+		.pipe(jscs.reporter("fail"));
+});
+
+gulp.task("test", ["jsonlint", "jshint", "jscs"]);
