6a9c74d6fda8894ed56924edf72e2e67da631c19
Balint Soos <balint.soos@edmdesigner.com>
2016-04-05 13:16:48 +0200

    RED-4630

---
+++ src/proxy/mongo.js
@@ -0,0 +1,81 @@
+/* 
+ * MongoDB proxy 
+ */
+
+var mongoose = require("mongoose");
+
+module.exports = function createMongoProxy(config) {
+	
+	config = config || {};
+
+	if (typeof config.mongoUrl !== "string") {
+		throw new Error("config.mongoUrl is mandatory");
+	}
+
+	if (typeof config.model !== "object") {
+		throw new Error("config.model is mandatory!");
+	}
+
+	var Model = config.model;
+
+	mongoose.connect(config.mongoUrl);
+
+	mongoose.connection.on("error", function() {
+		console.error("Mongoose connection error");
+	});
+
+	mongoose.connection.on("open", function() {
+		console.log("Mongoose connection successful");
+	});
+
+	function read(query, callback) {
+		Model.find(function(err, result) {
+			if (err) {
+				return callback(err);
+			}
+
+			// TODO: filter result
+
+			callback(null, {
+				items: data,
+				count: data.length
+			});
+		});
+	}
+
+	function createOne(data, callback) {
+		Model.create(data, function(err) {
+			callback(err);
+		});
+	}
+
+	function readOneById(id, callback) {
+		Model.findById(id, function(err, result) {
+			if (err) {
+				return callback(err);
+			}
+
+			callback(null, result);
+		});
+	}
+
+	function updateOneById(id, newData, callback) {
+		Model.findByIdAndUpdate(id, newData, function(err) {
+			callback(err);
+		});
+	}
+
+	function destroyOneById(id, callback) {
+		Model.findByIdAndRemove(id, function(err) {
+			callback(err);
+		});
+	}
+
+	return {
+		read: read,
+		createOne: createOne,
+		readOneById: readOneById,
+		updateOneById: updateOneById,
+		destroyOneById: destroyOneById
+	};
+};
