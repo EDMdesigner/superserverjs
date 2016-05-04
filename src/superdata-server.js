0392d6092de1198a22ab99e1e54ef7faffbd7343
Balint Soos <balint.soos@edmdesigner.com>
2016-05-04 16:23:15 +0200

    RED-4730

--- src/superdata-server.js
+++ src/superdata-server.js
@@ -1,8 +1,8 @@
 module.exports = {
 	proxy: {
 		file: require("./proxy/file"),
-		mongo: require("./proxy/mongo")//,
-		//s3: require("./proxy/s3")
+		mongo: require("./proxy/mongo"),
+		s3: require("./proxy/s3")
 	},
 	router: {
 		crud: require("./router/crud"),
