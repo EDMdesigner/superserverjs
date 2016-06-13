3163f1c8a11db8a5d27bbbb4cd68bfc542e25688
derPuntigamer <daniel.dudola@gmail.com>
2016-06-13 16:16:34 +0200

    add: multiple midlewares in CRUD router

--- src/router/crud.js
+++ src/router/crud.js
@@ -2,7 +2,6 @@ var express = require("express");
 
 var checkProxy = require("../utils/checkProxy");
 var checkBelongsTo = require("../utils/checkBelongsTo");
-
 var objectify = require("../utils/objectify");
 var intify = require("../utils/intify");
 var createResponseHandler = require("../utils/responseHandler");
@@ -21,11 +20,7 @@ module.exports = function createCRUDRouter(config) {
 	var proxy = config.proxy;
 	var router = config.router || express.Router({mergeParams: true});
 
-	function empty(req, res, next) {
-		next();
-	}
-
-	router.get("/", config.preHooks.get ? config.preHooks.get : empty, function(req, res) {
+	function get(req, res) {
 		console.log(req.lol);
 
 		var query = {};
@@ -45,17 +40,49 @@ module.exports = function createCRUDRouter(config) {
 		} else {
 			proxy.read(query, req.filter, createResponseHandler(req, res));
 		}
-	});
+	}
 
-	router.post("/", config.preHooks.post ? config.preHooks.post : empty, function(req, res) {
+	var getParams = ["/"];
+
+	if (config.preHooks.get) {
+		if (typeof config.preHooks.get === "function") {
+			getParams.push(config.preHooks.get);
+		} else if (config.preHooks.get instanceof Array) {
+			getParams = getParams.concat(config.preHooks.get);
+		} else {
+			throw new Error("config.preHooks.get must be a function or Array");
+		}
+	}
+
+	getParams.push(get);
+	router.get.apply(router, getParams);
+
+
+	function post(req, res) {
 		if (config.postHooks && config.postHooks.post) {
 			proxy.createOne(req.body, req.filter, createResponseHandler(req, res, config.postHooks.post));
 		} else {
 			proxy.createOne(req.body, req.filter, createResponseHandler(req, res));
 		}
-	});
+	}
 
-	router.get("/:id", config.preHooks.getOne ? config.preHooks.getOne : empty, function(req, res) {
+	var postParams = ["/"];
+
+	if (config.preHooks.post) {
+		if (typeof config.preHooks.post === "function") {
+			postParams.push(config.preHooks.post);
+		} else if (config.preHooks.post instanceof Array) {
+			postParams = postParams.concat(config.preHooks.post);
+		} else {
+			throw new Error("config.preHooks.post must be a function or Array");
+		}
+	}
+
+	postParams.push(post);
+	router.post.apply(router, postParams);
+
+
+	function getOne(req, res) {
 		var id = req.params.id;
 
 		if (config.postHooks && config.postHooks.getOne) {
@@ -63,23 +90,71 @@ module.exports = function createCRUDRouter(config) {
 		} else {
 			proxy.readOneById(id, req.filter, createResponseHandler(req, res));
 		}
-	});
+	}
 
-	router.put("/:id", config.preHooks.put ? config.preHooks.put : empty, function(req, res) {
+	var getOneParams = ["/:id"];
+
+	if (config.preHooks.getOne) {
+		if (typeof config.preHooks.getOne === "function") {
+			getOneParams.push(config.preHooks.getOne);
+		} else if (config.preHooks.getOne instanceof Array) {
+			getOneParams = getOneParams.concat(config.preHooks.getOne);
+		} else {
+			throw new Error("config.preHooks.getOne must be a function or Array");
+		}
+	}
+
+	getOneParams.push(getOne);
+	router.get.apply(router, getOneParams);
+
+
+	function put(req, res) {
 		if (config.postHooks && config.postHooks.put) {
 			proxy.updateOneById(req.params.id, req.body, req.filter, createResponseHandler(req, res, config.postHooks.put));
 		} else {
 			proxy.updateOneById(req.params.id, req.body, req.filter, createResponseHandler(req, res));
 		}
-	});
+	}
 
-	router.delete("/:id", config.preHooks.delete ? config.preHooks.delete : empty, function(req, res) {
+	var putParams = ["/:id"];
+
+	if (config.preHooks.put) {
+		if (typeof config.preHooks.put === "function") {
+			putParams.push(config.preHooks.put);
+		} else if (config.preHooks.put instanceof Array) {
+			putParams = putParams.concat(config.preHooks.put);
+		} else {
+			throw new Error("config.preHooks.put must be a function or Array");
+		}
+	}
+
+	putParams.push(put);
+	router.put.apply(router, putParams);
+
+
+	function del(req, res) {
 		if (config.postHooks && config.postHooks.delete) {
 			proxy.destroyOneById(req.params.id, req.filter, createResponseHandler(req, res, config.postHooks.delete));
 		} else {
 			proxy.destroyOneById(req.params.id, req.filter, createResponseHandler(req, res));
 		}
-	});
+	}
+
+	var deleteParams = ["/:id"];
+
+	if (config.preHooks.delete) {
+		if (typeof config.preHooks.delete === "function") {
+			deleteParams.push(config.preHooks.delete);
+		} else if (config.preHooks.delete instanceof Array) {
+			deleteParams = deleteParams.concat(config.preHooks.delete);
+		} else {
+			throw new Error("config.preHooks.delete must be a function or Array");
+		}
+	}
+
+	deleteParams.push(del);
+	router.delete.apply(router, deleteParams);
+
 
 	return router;
 };
