3997ab93ebb2963f08ba686e89072f05823e18fe
derPuntigamer <daniel.dudola@gmail.com>
2017-04-11 12:54:42 +0200

    add: new hook handling for responseHandler #3 #30

--- src/utils/responseHandler.js
+++ src/utils/responseHandler.js
@@ -1,6 +1,8 @@
 var fileType = require("file-type");
 
-module.exports = function createResponseHandler(req, res, postHook) {
+
+
+function createResponseHandler(req, res, postHook) {
 
 	return function handleResponse(err, result) {
 		if (err) {
@@ -17,7 +19,7 @@ module.exports = function createResponseHandler(req, res, postHook) {
 			if (fType) {
 				res.set("Content-Type", fType.mime);
 			}
-			
+
 			res.send(result);
 			if (postHook) {
 				postHook(null, result);
@@ -30,7 +32,19 @@ module.exports = function createResponseHandler(req, res, postHook) {
 		if (postHook) {
 			postHook(null, result);
 		}
-		
+
 		return;
 	};
+}
+
+module.exports = function createResponseHandlerWithHooks(config, req, res, hook) {
+	if (config.postHooks && config.postHooks[hook]) {
+		var responseHandler = createResponseHandler(
+			req, res, config.postHooks[hook]
+		);
+	} else {
+		var responseHandler = createResponseHandler(req, res);
+	}
+
+	return responseHandler;
 };
